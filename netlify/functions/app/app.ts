ed948ee5b4b6587319df217345f4c27e88a7e9c6
FabioReis <fabiojrodrigues.r@gmail.com>
2022-06-09 17:33:45 +0900

    test functions

--- netlify/functions/app/app.ts
+++
@@ -1,7 +0,0 @@
-import serverless from 'serverless-http';
-import '../../../server/common/env';
-import Server from '../../../server/common/server';
-import routes from '../../../server/routes';
-
-const app = new Server().router(routes).getApp();
-module.exports.handler = serverless(app);
